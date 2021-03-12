a9639935406b9ffacbac547a1d47a0fca79ecea1
ExplodedRiot <alive_ananda@yahoo.com>
2021-03-12 16:01:14 +0700

    edit route

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+    Route::get('/', function () {    return view('index');});
     Route::get('/about', function () {return view('about');});->name('about');
     Route::get('/contact', function () {return view('about');});->name('contact');
     Route::get('/exchange', function () {return view('about');});->name('exchange');
     Route::get('/home', function () {return view('home');});->name('home');
-    Route::get('/index', function () {return view('index');});->name('index');
     Route::get('/new', function () {return view('new');});->name('new');
     Route::get('/services', function () {return view('services');});->name('services');
     Route::get('/welcome', function () {return view('welcome');});->name('welcome');
